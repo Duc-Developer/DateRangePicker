13b69de8ca2fc87d0650f3d6d199406e000537de
DucTT <mrtranduc1994@outlook.com>
2021-07-31 22:39:27 +0700

    first clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DateRangePickers from "./DateRangePicker";
+import { DateRange } from "./DateRangePicker/Utilities/types";
 
 function App() {
+  const handleChange = (dateRange: DateRange) => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateRangePickers open onChange={handleChange} />
     </div>
   );
 }
