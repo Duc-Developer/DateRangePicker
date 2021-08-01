b8d00b4815c2049e46a803f74226b82767666846
DucTT <mrtranduc1994@outlook.com>
2021-08-01 18:22:31 +0700

    add option

--- src/MiniDateRange/index.tsx
+++ src/MiniDateRange/index.tsx
@@ -1,11 +1,23 @@
+import { Button } from "@material-ui/core";
 import React, { useState } from "react";
 
 import DateRangePickers from "./DateRangePicker";
 import { DateRange } from "./DateRangePicker/Utilities/types";
+import { addDays } from "./DateRangePicker/Helpers";
 
 const now = new Date();
 const initialDateRange = {
   startDate: now,
+  endDate: now,
+};
+
+const dateRange7last = {
+  startDate: addDays(now, -7),
+  endDate: now,
+};
+
+const dateRange30last = {
+  startDate: addDays(now, -30),
   endDate: now,
 };
 function MiniDateRange() {
@@ -18,7 +30,32 @@ function MiniDateRange() {
       setDates(initialDateRange);
     }
   };
-  return <DateRangePickers initialDateRange={dates} onChange={handleChange} />;
+
+  return (
+    <div style={{ display: "flex" }}>
+      <Button
+        onClick={() => {
+          setDates(dateRange7last);
+        }}
+        variant="contained"
+        color="primary"
+        style={{ marginRight: 8 }}
+      >
+        Last 7 days
+      </Button>
+      <Button
+        onClick={() => {
+          setDates(dateRange30last);
+        }}
+        variant="contained"
+        color="secondary"
+        style={{ marginRight: 8 }}
+      >
+        Last 30 days
+      </Button>
+      <DateRangePickers initialDateRange={dates} onChange={handleChange} />
+    </div>
+  );
 }
 
 export default MiniDateRange;
